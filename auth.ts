5c3d7101d605cd1c9a15118e42a2fbf8fc203385
Ameer Hasan <ameer.hasan.dev@gmail.com>
2024-09-14 15:02:29 -0400

    updated Auth

--- auth.ts
+++ auth.ts
@@ -1,8 +1,20 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import authConfig from "./auth.config";
+import { cert } from "firebase-admin/app";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY,
+    }),
+  }),
+  session: { strategy: "jwt" },
+  ...authConfig,
   providers: [Google, GitHub],
   pages: {
     signIn: "/auth/signin",
